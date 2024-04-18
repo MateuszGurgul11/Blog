5452ad9b8de93ed6d410fd34e13fee513d41102b
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-04-18 14:33:03 +0200

    Dodanie systemu logowania

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, redirect, url_for, flash, abort
+from flask import render_template, request, redirect, url_for, flash, session
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
 
 @app.route("/")
 def index():
@@ -28,3 +28,26 @@ def manage_entry(entry_id):
          errors == form.errors
          return render_template("entry_form.html", form=form, errors=errors)
    return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get("next")
+   if request.method == 'POST':
+      if form.validate_on_submit():
+         session['logged_in'] = True
+         session.permanent = True
+         flash('Jestes teraz zalogowany!', 'success')
+         return redirect(next_url or url_for('index'))
+      else:
+         errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+      session.clear()
+      flash("Zostales wylogowany!", "success")
+   return redirect(url_for("index"))
